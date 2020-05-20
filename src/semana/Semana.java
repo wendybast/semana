43bade36f1dfd0a08ea883716ce50735f39b4333
wendybast <wenbastidas@gmail.com>
2020-05-20 20:15:52 -0300

    2do commit
    se agrego //test

--- src/semana/Semana.java
+++ src/semana/Semana.java
@@ -16,6 +16,7 @@ public class Semana {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //test
         int numDia;
         numDia=3;
         
